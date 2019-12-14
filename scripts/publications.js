ee6ac5fe6e1b0110603c95f026b1745f811075fb
Joris Guerin <joris.guerin@isabo.ai>
2019-12-14 15:43:10 -0300

    transferred new version

---
+++ scripts/publications.js
@@ -0,0 +1,166 @@
+var mypubli = '<h2>Publications</h2>\
+<table width="100%" align="center" border="0" cellspacing="0" cellpadding="20"> \
+<tr> \
+  <td width="25%" valign="top"> <h2>2019</h2></td> \
+  <td width="75%" valign="top"> \
+  <p align="justify"> \
+      <b><papertitle>A Model Based on LSTM Neural Networks to \
+  Identify Five Different Types of Malware</papertitle></b> \
+  <br> \
+  <a href="https://www.researchgate.net/profile/Eduardo_Andrade14">Eduardo Andrade</a>, \
+  <a href="http://www.ic.uff.br/index.php/pt/pessoas/168-docente?docente=36">José Viterbo</a>, \
+  <a href="http://www.ic.uff.br/index.php/pessoas/168-docente?docente=18">Cristina Vasconcelos</a>, \
+  Joris Guérin, \
+  <a href="http://www.ic.uff.br/index.php/pessoas/168-docente?docente=104">Flavia Bernardini</a> \
+  <br> \
+  <em>International Conference on Knowledge-Based and Intelligent Information & Engineering Systems (KES)</em>, 2019 \
+  <br> \
+  [<a href="https://pdf.sciencedirectassets.com/280203/1-s2.0-S1877050919X00149/1-s2.0-S1877050919313523/main.pdf?X-Amz-Security-Token=IQoJb3JpZ2luX2VjENz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJHMEUCICdVs1VXuhUGl5HtZtyxFxCPTic52Z4MiMatLbnnefy1AiEAkhGJwcryRu8o6bUe1ebA6z2GrUwdmKvrMkRu%2BMgWNJQqzwIINRACGgwwNTkwMDM1NDY4NjUiDO3MIWEJ2vA9hbrXKiqsAqQrnCY28%2Bsz%2BH1mP2kiWNQc9gkE8Mdj1cL3DUe2Va38KJHP8eToVF2QgMVah5tou0hMtyyG3IDszCYQAu9Jq3t6TXQc7f9xOXoVwNHVofSKTZEN0BOlWrl%2BPR8DFBJm0DcF7ARPIvnBgu9OOQ5%2BECXWts%2BMt2cICNPK1iaGoVQVGR2BTwXgpAvQast15dCBaUrSHTieIaHYrk8pQ2%2Fe2%2Fi71TClaIiaCuO7O%2B6Pe%2BDmzil5eH2t6hYCWuX1KZ9xaAJgJZWurQ%2F%2FCFqo4x54ItSTgfZOg5N7nnCFh3VfaU2Ypdc9wy8IVdnzcfuvvF5N2e2A%2FUxJ9fNZgX0ngzeO8r1n1WFieJ0dnD3Ese4%2BCGi1VIuUE9vkgQk92tXW3tc1FR%2BjMER8eizol73U4zCA28%2FvBTrQAg8044bPWrvem9zpC6RKQ0zInC1vZtXTz90VbEu9UTxFNZvcCr26RSb84UlfDMzpaCJAay6KsMWFSF5uZ%2Bdm7GYVc582JAeG%2BPhG34fJ6o6WH0o08kTEPBrEcpoE0QHyTqpN79rqMvWBqqe9%2Bcyp2lfBJ%2BhLJUab1BtI%2B04FbK%2BkoCqC8H6YXHufRQuSVHZH4OSbgwZRN4GzjAN4SHTWqs0GrUkUvX1EpbVFK4hGMeizazl4r4PDxPX0Owr0rcSn6UHYhKo55EVibNgj9dp9UUkFNOR3Wbm0morN5yo5aD%2FXeIhC8pm%2BGS%2BpazbBOJcRrELClg4ZQWm5yxUYIveH0nP9CuhwJWWohl3S5KMHyn8IolzNYGidfNSMtDTjHNRmcWjdWgavmFbMc4%2FWxJYOwKbY7RxLZjCLou7pFGO8s5KYz5ii6csfczeH0MUJdeBqWQ%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20191213T203704Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAQ3PHCVTY4SO36NGM%2F20191213%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=ee7055a3ba08303365184f85b3a1db06cb4c934d3e6032c58161d7f04c421792&hash=06bf7115bfbff5b2a61c6627280410c61855c089c25f1f2ddb5a3a83b8b3e454&host=68042c943591013ac2b2430a89b270f6af2c76d8dfd086a07176afe7c76c2c61&pii=S1877050919313523&tid=spdf-579750ca-c27c-4a7e-bab7-1131adbc12c8&sid=02b0369d2a8b614328281195321d7138219fgxrqa&type=client">PDF</a>] \
+  [<a href="./bib/kes19.txt">bibtex</a>] \
+  </p></td> \
+</tr> \
+<tr> \
+  <td width="25%" valign="top"> <h2>2018</h2></td> \
+  <td width="75%" valign="top"> \
+  <p align="justify"> \
+      <b><papertitle>Machine learning improvements for robotic applications in industrial context: \
+        Case study of autonomous sorting</papertitle></b> \
+  <br> \
+  Joris Guérin \
+  <br> \
+  <em>Ph.D. dissertation</em> (2018) \
+  <br> \
+  [<a href="documents/thesis.pdf">PDF</a>] \
+  [<a href="./bib/thesis.txt">bibtex</a>] \
+  [<a href="./presentations/pres_defense.pdf">presentation</a>] \
+  <br><br><br> \
+      <b><papertitle>Improving Image Clustering With Multiple Pretrained CNN Feature Extractors</papertitle></b> \
+  <br> \
+  Joris Guérin, <a href="https://homes.cs.washington.edu/~bboots/">Byron Boots</a> \
+  <br> \
+  <em>British Machine Vision Conference (BMVC)</em>, 2018 \
+  <br> \
+  [<a href="http://bmvc2018.org/contents/papers/0071.pdf">PDF</a>] \
+  [<a href="https://arxiv.org/abs/1807.07760">arXiv</a>] \
+  [<a href="./bib/bmvc18.txt">bibtex</a>] \
+  [<a href="./presentations/poster_bmvc18.pdf">poster</a>] \
+  <br><br><br> \
+      <b><papertitle>Semantically Meaningful View Selection</papertitle></b> \
+  <br> \
+  Joris Guérin, \
+  <a href="http://www.oliviergibaru.org/">Olivier Gibaru</a>, \
+  <a href="https://www.researchgate.net/profile/Eric_Nyiri">Éric Nyiri</a>, \
+  <a href="https://www.researchgate.net/profile/Thiery_Stephane">Stéphane Thiery</a>, \
+  <a href="https://homes.cs.washington.edu/~bboots/">Byron Boots</a> \
+  <br> \
+  <em>International Conference on Intelligent Robots and Systems (IROS)</em>, 2018 \
+  <br> \
+  [<a href="https://arxiv.org/pdf/1807.10303.pdf">PDF</a>] \
+  [<a href="https://arxiv.org/abs/1807.10303">arXiv</a>] \
+  [<a href="./bib/iros18.txt">bibtex</a>] \
+  [<a href="./presentations/pres_iros18.pdf">presentation</a>] \
+  [<a href="https://github.com/jorisguerin/SemanticViewSelection_dataset">data</a>] \
+  <br><br><br> \
+      <b><papertitle>Automatic Construction of Real-World Datasets for 3D Object Localization using Two Cameras</papertitle></b> \
+  <br> \
+  Joris Guérin \
+  <a href="http://www.oliviergibaru.org/">Olivier Gibaru</a>, \
+  <a href="https://www.researchgate.net/profile/Eric_Nyiri">Éric Nyiri</a>, \
+  <a href="https://www.researchgate.net/profile/Thiery_Stephane">Stéphane Thiery</a>, \
+  Jorge Palos</a> \
+  <br> \
+  <em>Industrial Electronics Conference (IECON)</em>, 2018 \
+  <br> \
+  [<a href="https://arxiv.org/pdf/1707.02978.pdf">PDF</a>] \
+  [<a href="https://arxiv.org/abs/1707.02978">arXiv</a>] \
+  [<a href="./bib/iecon18.txt">bibtex</a>] \
+  [<a href="./presentations/pres_iecon18.pdf">presentation</a>] \
+  <br><br><br> \
+      <b><papertitle>Unsupervised robotic sorting: Towards autonomous decision making robots</papertitle></b> \
+  <br> \
+  Joris Guérin, \
+  <a href="https://www.researchgate.net/profile/Thiery_Stephane">Stéphane Thiery</a>, \
+  <a href="https://www.researchgate.net/profile/Eric_Nyiri">Éric Nyiri</a>, \
+  <a href="http://www.oliviergibaru.org/">Olivier Gibaru</a>, \
+  <br> \
+  <em>International Journal of Artificial Intelligence and Applications (IJAIA)</em>, March 2018 \
+  <br> \
+  [<a href="https://arxiv.org/pdf/1804.04572.pdf">PDF</a>] \
+  [<a href="https://arxiv.org/abs/1804.04572">arXiv</a>] \
+  [<a href="./bib/ijaia18.txt">bibtex</a>] \
+  <br><br><br> \
+      <b><papertitle>CNN features are also great at unsupervised classification</papertitle></b> \
+  <br> \
+  Joris Guérin, \
+  <a href="http://www.oliviergibaru.org/">Olivier Gibaru</a>, \
+  <a href="https://www.researchgate.net/profile/Thiery_Stephane">Stéphane Thiery</a>, \
+  <a href="https://www.researchgate.net/profile/Eric_Nyiri">Éric Nyiri</a>, \
+  <br> \
+  <em>International Conference on Computer Science, Engineering and Applications (CCSEA)</em>, 2018 \
+  <br> \
+  [<a href="https://arxiv.org/pdf/1707.01700.pdf">PDF</a>] \
+  [<a href="https://arxiv.org/abs/1707.01700">arXiv</a>] \
+  [<a href="./bib/ccsea18.txt">bibtex</a>] \
+  [<a href="https://www.youtube.com/watch?v=NpZIwY3H-gE">video</a>] \
+  [<a href="https://github.com/jorisguerin/toolClustering_dataset">data</a>] \
+  </p></td> \
+</tr> \
+<tr> \
+  <td width="25%" valign="top"> <h2>2017</h2></td> \
+  <td width="75%" valign="top"> \
+  <p align="justify"> \
+      <b><papertitle>Clustering for different scales of measurement-the gap-ratio weighted k-means algorithm</papertitle></b> \
+  <br> \
+  Joris Guérin, \
+  <a href="http://www.oliviergibaru.org/">Olivier Gibaru</a>, \
+  <a href="https://www.researchgate.net/profile/Thiery_Stephane">Stéphane Thiery</a>, \
+  <a href="https://www.researchgate.net/profile/Eric_Nyiri">Éric Nyiri</a>, \
+  <br> \
+  <em>International Conference on Computer Science, Engineering and Information Technology (CCSEIT)</em>, 2017 \
+  <br> \
+  [<a href="https://arxiv.org/pdf/1703.07625.pdf">PDF</a>] \
+  [<a href="https://arxiv.org/abs/1703.07625">arXiv</a>] \
+  [<a href="./bib/ccseit17.txt">bibtex</a>] \
+  [<a href="./presentations/pres_ccseit17.pdf">presentation</a>] \
+  [<a href="https://www.youtube.com/watch?v=korkcYs1EHM&t=66s">video</a>] \
+</tr> \
+<tr> \
+  <td width="25%" valign="top"> <h2>2016</h2></td> \
+  <td width="75%" valign="top"> \
+  <p align="justify"> \
+      <b><papertitle>Locally optimal control under unknown dynamics with learnt cost function: application to industrial robot positioning</papertitle></b> \
+  <br> \
+  Joris Guérin \
+  <a href="http://www.oliviergibaru.org/">Olivier Gibaru</a>, \
+  <a href="https://www.researchgate.net/profile/Thiery_Stephane">Stéphane Thiery</a>, \
+  <a href="https://www.researchgate.net/profile/Eric_Nyiri">Éric Nyiri</a>, \
+  <br> \
+  <em>European Workshop on Advanced Control and Diagnosis (ACD) </em>, 2016 \
+  <br> \
+  [<a href="https://iopscience.iop.org/article/10.1088/1742-6596/783/1/012036/pdf">PDF</a>] \
+  [<a href="./bib/acd16.txt">bibtex</a>] \
+  <br><br><br> \
+    <b><papertitle>Learning local trajectories for high precision robotic tasks: Application to KUKA LBR iiwa Cartesian positioning</papertitle></b> \
+  <br> \
+  Joris Guérin, \
+  <a href="http://www.oliviergibaru.org/">Olivier Gibaru</a>, \
+  <a href="https://www.researchgate.net/profile/Eric_Nyiri">Éric Nyiri</a>, \
+  <a href="https://www.researchgate.net/profile/Thiery_Stephane">Stéphane Thiery</a>, \
+  <br> \
+  <em>Industrial Electronics Conference (IECON)</em>, 2016 \
+  <br> \
+  [<a href="https://arxiv.org/pdf/1701.01497.pdf">PDF</a>] \
+  [<a href="https://arxiv.org/abs/1701.01497">arXiv</a>] \
+  [<a href="./bib/iecon16.txt">bibtex</a>] \
+  [<a href="./presentations/pres_iecon16.pdf">presentation</a>] \
+  [<a href="https://www.youtube.com/watch?v=Ekda9q3vv6Y">video</a>] \
+  </p></td> \
+</tr> \
+</table>'
+
+$(document).ready(function(){
+  $("#publications").click(function(){
+    $(".content").empty();
+    $(".content").append(mypubli);
+  });
+});
