e3781e97cac09221468e89bfcaed8f4e99c661c4
Joris Guerin <joris.guerin@isabo.ai>
2019-12-15 16:05:41 -0300

    Added in press and videos

---
+++ scripts/inpress.js
@@ -0,0 +1,19 @@
+var myinpress = '<h2>In press</h2> \
+<ul> \
+  <li> \
+  <a href="https://artsetmetiers.fr/fr/actualites/lintelligence-artificielle-vue-par-joris-guerin-doctorant-sur-le-campus-arts-et-metiers"> \
+  L’intelligence artificielle vue par Joris Guerin, doctorant sur le campus Arts et Métiers de Lille<\a> \
+  </li> \
+  <li> \
+  <a href="https://artsetmetiers.fr/fr/actualites/de-lille-atlanta-le-doctorat-de-joris-guerin"> \
+  De Lille à Atlanta: Le doctorat de Joris Guérin<\a> \
+  </li> \
+</ul> \
+<br>'
+
+$(document).ready(function(){
+  $("#inpress").click(function(){
+    $(".content").empty();
+    $(".content").append(myinpress);
+  });
+});
